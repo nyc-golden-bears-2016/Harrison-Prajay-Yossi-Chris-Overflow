c59492ecb8526f30470f6eda3fa658ee340b2cb1
Harrison Zandri & Prajay Soni <pair+hzandri.prajaysoni@devbootcamp.com>
2016-07-20 19:50:06 -0400

    support comments and sorting

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -13,3 +13,28 @@ post "/answers" do
     redirect "/login"
   end
 end
+
+get '/answers/:id/new_comment' do
+  if !logged_in?
+    session[:new_post_message] = "You must login to comment."
+    redirect "/login"
+  end
+  @answer = Answer.find_by(id: params[:id])
+  if @answer
+    erb :'comments/new'
+  else
+    halt(404, "404!!!!!! Answer not found")
+  end
+end
+
+post '/answers/new_comment' do
+  answer = Answer.find_by(id: params[:answer_id])
+  comment = Comment.new(comment_body: params[:comment_body], user_id: current_user.id)
+  if comment.save
+    answer.comments << comment
+    redirect "/questions/#{answer.question.id}"
+  else
+    session[:form_error] = comment.errors.full_messages
+    redirect "/questions/#{answer.question.id}/new_comment"
+  end
+end
