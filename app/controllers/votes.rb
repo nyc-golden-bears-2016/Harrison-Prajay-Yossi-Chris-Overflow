0da52e073c265561baa17311909197ca57d1bee5
Harrison Zandri & Prajay Soni <pair+hzandri.prajaysoni@devbootcamp.com>
2016-07-20 16:16:16 -0400

    Add voting feature for questions

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -19,3 +19,25 @@ post "/downvote/answers" do
   end
   redirect "/questions/#{answer.question_id}"
 end
+
+post "/upvote/questions" do
+  question = Question.find(params[:question_id])
+  if logged_in?
+    already_voted = question.votes.where(user_id: session[:id])
+    question.votes << Vote.new({value: 1, user_id: current_user.id}) if already_voted.empty?
+  else
+    session[:vote_error] = "You must log in to vote"
+  end
+  redirect "/questions/#{question.id}"
+end
+
+post "/downvote/questions" do
+  question = Question.find(params[:question_id])
+  if logged_in?
+    already_voted = question.votes.where(user_id: current_user.id)
+    question.votes << Vote.new({value: -1, user_id: current_user.id}) if already_voted.empty?
+  else
+    session[:vote_error] = "You must log in to vote"
+  end
+  redirect "/questions/#{question.id}"
+end
