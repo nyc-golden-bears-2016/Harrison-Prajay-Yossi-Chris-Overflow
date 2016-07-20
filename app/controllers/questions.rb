083aba180ecb47b02e6b9a99ecd37d3230d1836c
Harrison Zandri & Prajay Soni <pair+hzandri.prajaysoni@devbootcamp.com>
2016-07-20 14:39:26 -0400

    Make controllers and views for questions and answers

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -2,3 +2,33 @@ get '/questions' do
   @questions = Question.all
   erb :"questions/index"
 end
+
+get '/questions/new' do
+  if current_user
+    erb :'questions/new'
+  else
+    session[:new_post_message] = "You must be logged in to ask a question"
+    redirect '/login'
+  end
+end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+  @question.user_id = session[:id]
+
+   if @entry.save
+    redirect "/question/#{@question.id}"
+  else
+    session[:form_error] = @entry.errors.full_messages
+    redirect "/question/new"
+  end
+end
+
+get '/questions/:id' do
+  @question = Question.find_by(id: params[:id])
+  if @question
+    erb :'questions/show'
+  else
+    halt(404, "Question not found")
+  end
+end
